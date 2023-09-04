fdb65bcf28241ff0e5b5cbecf73bd14200848b97
Muffet <samanthalricotta@gmail.com>
2023-09-04 13:03:17 +0300

    update pp

--- app/proposal.go
+++ app/proposal.go
@@ -2,8 +2,10 @@ package app
 
 import (
 	"cosmossdk.io/log"
+	"encoding/json"
 	abci "github.com/cometbft/cometbft/abci/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	txtypes "github.com/cosmos/cosmos-sdk/types/tx"
 )
 
 type ProposalHandler struct {
@@ -15,13 +17,23 @@ func (h *ProposalHandler) NewPrepareProposal() sdk.PrepareProposalHandler {
 	return func(ctx sdk.Context, req *abci.RequestPrepareProposal) (*abci.ResponsePrepareProposal, error) {
 		var proposalTxs [][]byte
 
-		for _, tx := range req.Txs {
+		for _, txBytes := range req.Txs {
+			tx := txtypes.Tx{}
 
-			proposalTxs = append(proposalTxs, tx)
+			err := json.Unmarshal(txBytes, &tx)
+			if err != nil {
+				return nil, err
+			}
+
+			messages := tx.GetMsgs()
+			for _, msg := range messages {
+				if sdk.MsgTypeURL(msg) == "Bid" {
+					h.logger.Info("Bid Found")
+					//	to be altered when bid added
+				}
+			}
+			proposalTxs = append(proposalTxs, txBytes)
 		}
-
-		return &abci.ResponsePrepareProposal{
-			Txs: proposalTxs,
-		}, nil
+		return nil, nil
 	}
 }
